f469f340e822cf0342e7771682a6d45b448110e4
! Sleepy <109904491+eepyfemboi@users.noreply.github.com>
2024-08-29 19:36:12 -0700

    made it automatically re enable picture in picture

--- content.js
+++ content.js
@@ -1,4 +1,10 @@
 (function() {
+  document.querySelectorAll('video').forEach(video => {
+    if (video.hasAttribute('disablePictureInPicture')) {
+        video.removeAttribute('disablePictureInPicture');
+    }
+  });
+
   window.addEventListener('beforeunload', function(event) {
     event.returnValue = 'Are you sure you want to leave this page?';
   });
