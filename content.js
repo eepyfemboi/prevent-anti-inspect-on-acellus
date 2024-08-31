9646581a9dbc4cb9f36551fbf063cb51877f221d
! Sleepy <109904491+eepyfemboi@users.noreply.github.com>
2024-08-30 20:42:25 -0700

    made it actually remove the disablePictureInPicture elements, added the homepage and signin page to ignored domains

--- content.js
+++ content.js
@@ -1,11 +1,19 @@
 (function() {
-  document.querySelectorAll('video').forEach(video => {
+  const ignoredDomains = [
+    "acellus.com",
+    "www.acellus.com",
+    "signin.acellus.com"
+  ]
+
+  if (!ignoredDomains.includes(window.location.hostname)) {
+    window.addEventListener('beforeunload', function(event) {
+      event.returnValue = 'Are you sure you want to leave this page?';
+    });
+  }
+
+  setTimeout(function(){document.querySelectorAll('video').forEach(video => {
     if (video.hasAttribute('disablePictureInPicture')) {
         video.removeAttribute('disablePictureInPicture');
     }
-  });
-
-  window.addEventListener('beforeunload', function(event) {
-    event.returnValue = 'Are you sure you want to leave this page?';
-  });
+  });}, 5000)
 })();
